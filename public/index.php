9d327eef7ef668d31d63010f15308e5d52d95fb8
contactmeroshan <87830296+contactmeroshan@users.noreply.github.com>
2022-05-13 14:53:07 +0530

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+	<li><a href="create.php"><strong>Create</strong></a> - add patient</li>
+	<li><a href="read.php"><strong>Read</strong></a> - find patient</li>
+	<li><a href="update.php"><strong>Update</strong></a> - edit patient</li>
+	<li><a href="delete.php"><strong>Delete</strong></a> - delete patient</li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
